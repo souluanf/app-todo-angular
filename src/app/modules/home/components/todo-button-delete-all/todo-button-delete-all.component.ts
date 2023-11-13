92591ce16d67f39ebde361875c63a5552e35b844
Luan Fernandes <hello@luanfernandes.dev>
2023-11-12 22:35:10 -0300

    feat(component): criação componente todo-button-delete-all/

---
+++ src/app/modules/home/components/todo-button-delete-all/todo-button-delete-all.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-todo-button-delete-all',
+  templateUrl: './todo-button-delete-all.component.html',
+  styleUrl: './todo-button-delete-all.component.scss'
+})
+export class TodoButtonDeleteAllComponent {
+
+}
